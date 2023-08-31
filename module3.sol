9b11b9bbfedda8d5dc35240a56343a53ccb43e82
sukant kumar <129366933+shresth19@users.noreply.github.com>
2023-08-31 18:14:47 +0530

    Update module3.sol

--- module3.sol
+++ module3.sol
@@ -1,34 +1,57 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+contract MyToken {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+    address public owner;
 
-contract MyToken is ERC20, ERC20Burnable, Ownable {
-    constructor() ERC20("MyToken", "MTK") {
-        _mint(msg.sender, 100 * 10 ** decimals());
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Burn(address indexed from, uint256 value);
+    event Mint(address indexed to, uint256 value);
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals,
+        uint256 _initialSupply
+    ) {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        totalSupply = _initialSupply * 10**uint256(decimals);
+        balanceOf[msg.sender] = totalSupply;
+        owner = msg.sender;
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
     }
 
-    // Custom burn function to burn a specific amount of tokens
-    function burnTokens(uint256 amount) external {
-        _burn(msg.sender, amount);
-    }
-
-    // Custom transfer function that burns a percentage of the transferred amount
-    function transferWithBurn(address recipient, uint256 amount, uint256 burnPercentage) external returns (bool) {
-        require(burnPercentage <= 100, "Burn percentage must be between 0 and 100");
-        
-        uint256 burnAmount = (amount * burnPercentage) / 100;
-        uint256 transferAmount = amount - burnAmount;
-        
-        _burn(msg.sender, burnAmount);
-        _transfer(msg.sender, recipient, transferAmount);
-        
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        require(_to != address(0), "Invalid address");
+        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
         return true;
+    }
+
+    function burn(uint256 _value) public {
+        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
+        balanceOf[msg.sender] -= _value;
+        totalSupply -= _value;
+        emit Burn(msg.sender, _value);
+    }
+
+    function mint(address _to, uint256 _value) public onlyOwner {
+        require(_to != address(0), "Invalid address");
+        totalSupply += _value;
+        balanceOf[_to] += _value;
+        emit Mint(_to, _value);
     }
 }
