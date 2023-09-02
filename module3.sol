3e62904378ef28e06205365d777ed54ec2bb43bd
sukant kumar <129366933+shresth19@users.noreply.github.com>
2023-09-02 18:14:25 +0530

    Update module3.sol

--- module3.sol
+++ module3.sol
@@ -1,57 +1,30 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
-
-contract MyToken {
-    string public name;
-    string public symbol;
-    uint8 public decimals;
+// SPDX-License-Identifier: UNLICENCED
+pragma solidity >=0.7.0 <0.9.0;
+contract Token {
+    string public name = "TOKEN";
+    string public symbol = "TKN";
     uint256 public totalSupply;
-    mapping(address => uint256) public balanceOf;
+    mapping(address => uint256) public balances;
     address public owner;
-
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Burn(address indexed from, uint256 value);
-    event Mint(address indexed to, uint256 value);
-
-    constructor(
-        string memory _name,
-        string memory _symbol,
-        uint8 _decimals,
-        uint256 _initialSupply
-    ) {
-        name = _name;
-        symbol = _symbol;
-        decimals = _decimals;
-        totalSupply = _initialSupply * 10**uint256(decimals);
-        balanceOf[msg.sender] = totalSupply;
+    constructor() {
         owner = msg.sender;
     }
-
     modifier onlyOwner() {
-        require(msg.sender == owner, "Only the owner can call this function");
+        require(msg.sender == owner, "Only owner can mint tokens");
         _;
     }
-
-    function transfer(address _to, uint256 _value) public returns (bool) {
-        require(_to != address(0), "Invalid address");
-        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
-        balanceOf[msg.sender] -= _value;
-        balanceOf[_to] += _value;
-        emit Transfer(msg.sender, _to, _value);
-        return true;
+    function mint(address to, uint256 amount) public onlyOwner {
+        totalSupply += amount;
+        balances[to] += amount;
     }
-
-    function burn(uint256 _value) public {
-        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
-        balanceOf[msg.sender] -= _value;
-        totalSupply -= _value;
-        emit Burn(msg.sender, _value);
+    function transfer(address to, uint256 amount) public {
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
     }
-
-    function mint(address _to, uint256 _value) public onlyOwner {
-        require(_to != address(0), "Invalid address");
-        totalSupply += _value;
-        balanceOf[_to] += _value;
-        emit Mint(_to, _value);
+    function burn(uint256 amount) public {
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+        balances[msg.sender] -= amount;
+        totalSupply -= amount;
     }
 }
