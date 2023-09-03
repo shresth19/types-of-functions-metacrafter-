1802f5d84f8b40893844b4e7fed68da4a7565db9
sukant kumar <129366933+shresth19@users.noreply.github.com>
2023-09-03 16:38:52 +0530

    Update module3.sol

--- module3.sol
+++ module3.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyToken is ERC20, ERC20Burnable, Ownable {
-    constructor() ERC20("SHRESTH", "SHR") {
+    constructor() ERC20("SHRESTH", "okok") {
         _mint(msg.sender, 1000 * 10 ** decimals());
     }
 
